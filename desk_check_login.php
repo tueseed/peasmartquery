a48d464f0996b3aaf88c2f2733b9a2fbbe41fd93
Nutthapong <tue_seed@hotmail.com>
2019-05-08 20:20:02 +0700

    dgzvdv

--- desk_check_login.php
+++ desk_check_login.php
@@ -14,7 +14,7 @@
 	$result = mysqli_query($conn,$sql);
 	$objResult = mysqli_fetch_array($result);
 	echo $objResult["user"];
-	/*if(!$objResult)
+	if(!$objResult)
 	{
 		echo "ชื่อผู้ใช้ หรือรหัสผ่านไม่ถูกต้อง หรือไม่มีในระบบ " ;
 		echo '<br>กลับสู่หน้า  <a href = "http://nutt-i.com/psqv2/desk_login.php"> เข้าสู่ระบบ </a>';
@@ -25,8 +25,8 @@
 		session_start();
 		$_SESSION["USER"] = $objResult["user"];
 		echo '<script type="text/javascript">';
-      	echo 'window.location.href="index.php";';
-      	echo '</script>';
+    echo 'window.location.href="index.php";';
+  	echo '</script>';
 	}
 	/*ob_end_flush();*/
 	
