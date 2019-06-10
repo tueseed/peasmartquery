716813f98ddc0adcdeedbbe00889d5cd87069ef6
Nutthapong <tue_seed@hotmail.com>
2019-06-10 10:39:01 +0700

    fksld;kfs;df

--- check_uid.php
+++ check_uid.php
@@ -1,29 +1,28 @@
-<html>
-<head>
-	<title>PSQ V.2</title>
-</head>
-<body>
-<?php
-	require('connect-db.php');
-	$uid = $_POST["useridfield"];
-	$sql_chk_uid = "SELECT * FROM tbl_psq_search_staff WHERE uid ='".$uid."'";
-	$query_uid = mysqli_query($conn,$sql_chk_uid);
-	$nums = mysqli_num_rows($query_uid);
-	if($nums == 0)
-	{
-		echo '<script type="text/javascript">';
-		echo 'window.location.href="https://nutt-i.com/psqv2/regist_liff_search.php?uid='.$uid.'";';
-		echo '</script>';
-		break;
-	}
-	else if ($nums <> 0)
-	{
-		echo '<script type="text/javascript">';
-		echo 'window.location.href="https://nutt-i.com/psqv2/psq_liff_search.php";';
-		echo '</script>';
-		break;
-	}
-	
-?>
-</body>
+<html>
+<head>
+	<title>PSQ V.2</title>
+</head>
+<body>
+<?php
+	require('connect-db.php');
+	$uid = $_POST["useridfield"];
+	$sql_chk_uid = "SELECT * FROM tbl_psq_search_staff WHERE uid ='".$uid."'";
+	$query_uid = mysqli_query($conn,$sql_chk_uid);
+	$nums = mysqli_num_rows($query_uid);
+	if($nums == 0)
+	{
+		echo '<script type="text/javascript">';
+		echo 'window.location.href="https://nutt-i.com/psqv2/regist_liff_search.php?uid='.$uid.'";';
+		echo '</script>';
+		break;
+	}
+	else if ($nums <> 0)
+	{
+		echo '<script type="text/javascript">';
+		echo 'window.location.href="https://nutt-i.com/psqv2/psq_liff_search.php";';
+		echo '</script>';
+		break;
+	}
+?>
+</body>
 </html>
